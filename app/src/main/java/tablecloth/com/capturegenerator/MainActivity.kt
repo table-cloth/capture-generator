361084daef14906d334a74287d2f38cf750d6357
Minami <table.cloth.03@gmail.com>
2018-05-04 16:13:18 +0700

    remove unneeded codes #2

--- app/src/main/java/tablecloth/com/capturegenerator/MainActivity.kt
+++ app/src/main/java/tablecloth/com/capturegenerator/MainActivity.kt
@@ -12,30 +12,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var screenCapture: ScreenCapture
-//    companion object {
-//        private const val REQUEST_CAPTURE = 1
-//        var projection: MediaProjection?  = null
-//    }
-//
-//    private lateinit var mediaProjectionManager: MediaProjectionManager
-//
     private val TAG = "MainActivity"
-//    private val REQUEST_SCREEN_CAPTURE = 1
-//
-//    private lateinit var mediaProjectionManager: MediaProjectionManager
-//    private lateinit var permissionIntent: Intent
-//
-//    private var mediaProjection: MediaProjection? = null
-//    private var virtualDisplay: VirtualDisplay? = null
-//
-//    private var imageReader: ImageReader? = null
-//    private var uiHandler: Handler? = null
-//
-//
-////    private lateinit var screenCapture: ScreenCapture = null
-//
-//    init {
-//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,145 +35,18 @@ class MainActivity : AppCompatActivity() {
                     completeCallback = {
                         Log.d(TAG, "Capture complete.")
                     },
-                    config = ScreenCapture.CaptureConfig(1000L, 3000L, 3))
+                    config = ScreenCapture.CaptureConfig(100L, 3000L, 3))
         })
 
-//        uiHandler = Handler()
-//
-//        mediaProjectionManager = getSystemService(android.content.Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
-//        permissionIntent = mediaProjectionManager.createScreenCaptureIntent()
-//
-//        Log.d(TAG, "Start activity.")
-//
-//
-//        Log.d(TAG, "Start permission intent.")
-//        startActivityForResult(permissionIntent, REQUEST_SCREEN_CAPTURE)
-//
-////        screenCapture.initialize(onActivityResult = )
-//
-//        capture_image.setOnClickListener({ _ ->
-//            Log.d(TAG, "Start permission intent by image.")
-//            startActivityForResult(permissionIntent, REQUEST_SCREEN_CAPTURE)
-//        })
-//
-//        capture.setOnClickListener({ _ ->
-////            capture_image.setImageBitmap(updateCaptureImage())
-//
-//            Log.d(TAG, "Start permission intent by button.")
-//            startActivityForResult(permissionIntent, REQUEST_SCREEN_CAPTURE)
-//        })
-//
-//
-//        mediaProjectionManager = getSystemService(Service.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
-//        startActivityForResult(mediaProjectionManager.createScreenCaptureIntent(), REQUEST_CAPTURE)
     }
-
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-
+        super.onActivityResult(requestCode, resultCode, data)
         screenCapture.onActivityResult(requestCode, resultCode, data)
-
-////        super.onActivityResult(requestCode, resultCode, data)
-//
-//        if(requestCode != REQUEST_SCREEN_CAPTURE) {
-//            return
-//        }
-//
-//        if(resultCode == Activity.RESULT_OK) {
-//            Log.d(TAG, "SUCCESSSSSSS")
-//            Snackbar.make(activity_root, "Permission is ON", Snackbar.LENGTH_SHORT).show()
-//        } else {
-//            Snackbar.make(activity_root, "Permission is OFF", Snackbar.LENGTH_SHORT).show()
-//            return
-//        }
-//
-//        mediaProjection = mediaProjectionManager.getMediaProjection(resultCode, intent)
-//
-//        if(mediaProjection == null) {
-//            Snackbar.make(activity_root, "mediaProjection is NULL...", Snackbar.LENGTH_SHORT).show()
-//            return
-//        }
-//
-//
-//        // Display info
-//        val metrics = resources.displayMetrics
-//        val width = metrics.widthPixels
-//        val height = metrics.heightPixels
-//        val density = metrics.densityDpi
-//
-//        // Setup virtual display
-//        imageReader = ImageReader.newInstance(
-//                width, height, PixelFormat.RGBA_8888, 2)
-//        virtualDisplay = mediaProjection!!.createVirtualDisplay(
-//                "Capturing Display...!", width, height, density,
-//                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
-//                imageReader!!.surface,
-//                null, null)
-//
-////        Handler().postDelayed(Runnable {
-////            uiHandler!!.post(Runnable {
-////                capture_image.setImageBitmap(updateCaptureImage())
-////            })
-////        }, 1000)
-//
     }
-//
-//    private fun updateCaptureImage(): Bitmap? {
-////        capture_image
-//        val image = imageReader?.acquireLatestImage()
-//        if(image == null) {
-//            Log.e(TAG, "FAIL TO CAPUTRE IMAGE")
-//            Snackbar.make(activity_root, "Fail update capture image...", Snackbar.LENGTH_SHORT).show()
-//            return null
-//        }
-//
-//        val metrics = resources.displayMetrics
-//
-//        val planes = image.planes
-//        val buffer = planes[0].buffer
-//
-//        val pixelStide = planes[0].pixelStride
-//        val rowStride = planes[0].rowStride
-//        val rowPadding = rowStride - pixelStide * metrics.widthPixels
-//
-//        // Create bitmap from buffer
-//        val bitmap = Bitmap.createBitmap(
-//                metrics.widthPixels + rowPadding / pixelStide,
-//                metrics.heightPixels,
-//                Bitmap.Config.ARGB_8888)
-//        bitmap.copyPixelsFromBuffer(buffer)
-//        image.close()
-//
-//        return bitmap
-//    }
-//
-//
-//    override fun onPause() {
-//        Log.d(TAG, "OnPause")
-//        Snackbar.make(activity_root, "Release virtual display.", Snackbar.LENGTH_SHORT).show()
-//        super.onPause()
-//    }
 
     override fun onDestroy() {
         screenCapture.stopCapture()
-//        virtualDisplay?.release()
-//        Log.d(TAG, "OnDestroy")
-//        Snackbar.make(activity_root, "Release virtual display.", Snackbar.LENGTH_SHORT).show()
         super.onDestroy()
     }
-
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        if(requestCode == REQUEST_CAPTURE) {
-//            if(resultCode == Activity.RESULT_OK) {
-//                projection = mediaProjectionManager.getMediaProjection(resultCode, data)
-////                val intent = Intent(this, CaptureService::class.java)
-////                startService(intent)
-//                Toast.makeText(this, "capture start service", Toast.LENGTH_SHORT).show()
-//            } else {
-//                projection = null
-//                Toast.makeText(this, "capture error", Toast.LENGTH_SHORT).show()
-//            }
-//        }
-//        finish()
-//    }
 }
